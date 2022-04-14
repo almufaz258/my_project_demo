5fb55e87a6492c8f3c0eaa0d81d55a32eaa32633
Albina M <103380907+almufaz258@users.noreply.github.com>
2022-04-15 00:48:06 +0500

    добавление switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_project_demo/switches_demo.dart';
 import 'text_demo_screen.dart';
 
 void main() {
-  runApp(const TextDemoScreen());
+  runApp(const SwitchesDemoScreen());
 }
 
---
+++ lib/switches_demo.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class SwitchesDemoScreen extends StatefulWidget {
+  const SwitchesDemoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SwitchesDemoScreen> createState() => _SwitchesDemoScreenState();
+}
+
+enum SkillLevel{junior, middle, senior}
+
+class _SwitchesDemoScreenState extends State<SwitchesDemoScreen> {
+  bool _checked = false;
+  bool _confirmAgreement = true;
+  SkillLevel? _skillLevel = SkillLevel.junior;
+
+  void _onCheckedChange(bool? val) {
+    setState(() {
+      _checked = !_checked;
+    });
+  }
+
+  void _onSkillLevelChanged(SkillLevel? value) {
+    setState(() {
+      _skillLevel = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(),
+            body: Center(
+                child: Column(children: [
+              Row(
+                children: [
+                  Checkbox(value: _checked, onChanged: _onCheckedChange),
+                  const Text('Выбор'),
+                ],
+              ),
+              CheckboxListTile(
+                  title: const Text('Принять условия соглашения'),
+                  value: _confirmAgreement, onChanged: (val) {
+                  setState(() {
+                    _confirmAgreement = !_confirmAgreement;
+                });
+              }),
+            Row(
+              children: [
+                Switch(value: _checked, onChanged: _onCheckedChange),
+                const Text('Включить'),
+              ],
+            ),
+            SwitchListTile(
+               title: const Text('Включить'),
+               value: _checked,
+               onChanged: _onCheckedChange
+            ),
+            const Text('Уровень навыков: '),
+            RadioListTile<SkillLevel>(
+              title: const Text('junior'),
+              value: SkillLevel.junior,
+              groupValue: _skillLevel,
+                onChanged: _onSkillLevelChanged,
+            ),
+            RadioListTile<SkillLevel>(
+            title: const Text('middle'),
+            value: SkillLevel.middle,
+            groupValue: _skillLevel,
+              onChanged: _onSkillLevelChanged,
+            ),
+            RadioListTile<SkillLevel>(
+            title: const Text('senior'),
+            value: SkillLevel.senior,
+            groupValue: _skillLevel,
+              onChanged: _onSkillLevelChanged,
+            ),
+    ]))));
+  }
+}
